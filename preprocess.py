5bb68ba1d4b01ef80931d3bdd8bc5b3a50ca573a
jiwon <jjwon4086@gmail.com>
2022-12-04 01:33:29 +0900

    add preprocessing test dataset

--- preprocess.py
+++ preprocess.py
@@ -1,34 +1,52 @@
 import pandas as pd
 import json
 import config as Config
-
-group = ['dress', 'shirt', 'toptee']
+import numpy as np
 
 # mode: train, val, test
 def preprocess_dataset(mode="train"):
-    captions = []
-    for i in range(3):
-        with open(f"{Config.captions_path}/cap.{group[i]}.{mode}.json", 'r') as c:
-            captions += json.loads(c.read())
+    if mode == "test":
+        splits = []
+        for group in ["test", "train", "val"]:
+            with open(f"{Config.split_path}/split.shirt.{group}.json", 'r') as s:
+                splits += json.loads(s.read())
 
-    target = []
-    candidate = []
-    caption = []
-    for i in range(len(captions)):
-        for j in range(2):
-            target.append(captions[i]["target"])
-            candidate.append(captions[i]["candidate"])
-            caption.append(captions[i]["captions"][j])
-    
-    df = pd.DataFrame({
-        "target": target,
-        "candidate": candidate,
-        "caption": caption
-    })
+        caption = [""]*len(splits)
+        df = pd.DataFrame({
+            "target": splits,
+            "candidate": splits,
+            "caption": caption
+        })
+        
+    else:
+        captions = []
+        with open(f"{Config.captions_path}/cap.shirt.{mode}.json", 'r') as c:
+            captions = json.loads(c.read())
+
+        target = []
+        candidate = []
+        caption = []
+        for i in range(len(captions)):
+            for j in range(2):
+                target.append(captions[i]["target"])
+                candidate.append(captions[i]["candidate"])
+                caption.append(captions[i]["captions"][j])
+        
+        df = pd.DataFrame({
+            "target": target,
+            "candidate": candidate,
+            "caption": caption
+        })
 
     # Randomly shuffle
     return df.sample(frac=1).reset_index(drop=True)
 
 # Test
 if __name__ == '__main__':
-    print(preprocess_dataset().sample(n=5))
+    dataset = preprocess_dataset()
+    print(dataset.sample(n=5))
+    print(dataset['target'].values)
+    print(dataset['candidate'].values)
+    print(np.concatenate((dataset['target'].values, dataset['candidate'].values)))
+    test = preprocess_dataset("test")
+    print(test.sample(n=5))
